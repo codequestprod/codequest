759e04af3e70f9da6bc820d94c26a8aaf89c3dbd
phil <phil.ovesnov@gmail.com>
2026-07-26 21:32:56 -0700

    Remove debug + leave only 1 db.create_all

--- app.py
+++ app.py
@@ -351,9 +351,6 @@ def profile():
 # RUN
 # =========================
 
-with app.app_context():
-    print(os.getenv("DATABASE_URL"))
-    db.create_all()
     
 if __name__ == "__main__":
     app.run(debug=True)
